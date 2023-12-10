785d2f0dc271fa04cd19d9103bd3e4ca2b207bfd
aherman21 <hermansalexander2@gmail.com>
2023-12-10 18:55:05 +0100

    handlers for db done and models added

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-const static = require('static')
+const Feedback = require('./models/feedback')
+const HighScore = require('./models/highScore')
+const { nextTick } = require('process')
+const feedback = require('./models/feedback')
 
 //define middlewares
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: error.message })
+    }
+    next(error)
+}
+
+
+
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
+//morgan logs custom formated request data to console
 app.use(morgan(function (tokens, req, res) {
     return [
         tokens.method(req, res),
@@ -22,86 +39,70 @@ app.use(morgan(function (tokens, req, res) {
     ].join(' ')
 }))
 
-let feedbacks = [
-    {
-        "id": 1702040689696,
-        "name": "moimoi",
-        "message": "aaa",
-        "date": "2023-12-08T13:04:49.696Z",
-        "isPublic": true
-      },
-      {
-        "id": 1702040698528,
-        "name": "aasfasf",
-        "message": "waw",
-        "date": "2023-12-08T13:04:58.528Z",
-        "isPublic": true
-      },
-      {
-        "id": 1702040740128,
-        "name": "hei",
-        "message": "aa",
-        "date": "2023-12-08T13:05:40.128Z",
-        "isPublic": true
-      }
-  ]
-
-  let highScores = [
-    {
-      "nickName": "asdads",
-      "score": 1,
-      "id": 1
-    },
-    {
-      "nickName": "testi",
-      "score": 3,
-      "id": 2
-    },
-    {
-      "nickName": "testi2",
-      "score": 2,
-      "id": 3
-    }
-]
+app.use(errorHandler)
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
 
+// getting all feedbacks
 app.get('/api/feedbacks', (req, res) => {
-    res.json(feedbacks)
+    Feedback.find({}).then(feedbacks => {
+        res.json(feedbacks)
+    })
 })
 
-app.get('/api/feedbacks/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const feedback = feedbacks.find(feedback => feedback.id === id)
-    if (feedback) {
-        res.json(feedback)
-    } else {
-        res.status(404).end()
-    }
-})
+// delete feedback
+app.delete('/api/feedbacks/:id', (req, res, next) => {
+    Feedback.findByIdAndDelete(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 
-app.delete('/api/feedback/:id', (req, res) => {
-    const id = Number(req.params.id)
-    feedbacks = feedbacks.filter(feedback => feedback.id !== id)
-    res.status(204).end()
-})
 
+})
+// posting a feedback
 app.post('/api/feedbacks', (req, res) => {
-    const feedback = req.body
-    console.log(feedback)
-    res.json(feedback)
-})
+    const body = req.body
 
+    if (body.message === undefined) {
+        return res.status(400).json({ error: 'name missing' })
+    }
+
+    const feedback = new Feedback({
+        name: body.name,
+        message: body.message,
+        date: new Date(),
+        isPublic: body.isPublic || false
+    })
+
+    feedback.save().then(savedFeedback => {
+        res.json(savedFeedback)
+    })
+})
+// get all highscores
 app.get('/api/highScores', (req, res) => {
-    res.json(highScores)
+    HighScore.find({}).then(highScores => {
+        res.json(highScores)
+    })
 })
-
+// post a highscore - this happens when the game is over
 app.post('/api/highScores', (req, res) => {
-    const highScore = req.body
-    console.log(highScore)
-    res.json(highScore)
+    const body = req.body
+
+    if (body.score === undefined) {
+        return res.status(400).json({ error: 'score missing' })
+    }
+
+    const highScore = new HighScore({
+        nickName: body.nickName,
+        score: body.score,
+    })
+
+    highScore.save().then(savedHighScore => {
+        res.json(savedHighScore)
+    })
 })
 
 const PORT = process.env.PORT || 3002
